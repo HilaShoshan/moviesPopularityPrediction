f73656aed79452139ad179b7c2ce109481b795d5
Hila Shoshan <hilashoshan0605@gmail.com>
2020-12-08 19:33:13 +0200

    added funcrion to arrange

--- ArrangeData.py
+++ ArrangeData.py
@@ -50,14 +50,20 @@ class ArrangeData:
         return norm_df_x, norm_df_y
 
 
+    def convert_with_nltk(self):
+        pass
+
+
     def arrange(self):
         self.split_date()
         self.convert_to_numeric()  # We will improve it later so that the values will be numerically better
         self.fix_nulls()
-        # print(df_x.isnull().sum())
+        # print(self.df_x.isnull().sum())
+        """
         for col in self.df_x.columns:
             if not is_numeric_dtype(self.df_x[col]):
                 # print(col)
                 del self.df_x[col]
+        """
         norm_df_x, norm_df_y = self.normalize()
         return norm_df_x, norm_df_y
--- Main.py
+++ Main.py
@@ -13,8 +13,8 @@ columns = ['runtime', 'production_companies', 'genres', 'revenue', 'original_lan
 
 
 def compute_error(y_test, y_pred):
-    print(metrics.accuracy_score(y_test, y_pred))
-    print(metrics.confusion_matrix(y_test, y_pred))
+    #print(metrics.accuracy_score(y_test, y_pred))
+    #print(metrics.confusion_matrix(y_test, y_pred))
     print(mean_squared_error(y_test, y_pred))
 
 
@@ -27,14 +27,23 @@ def main():
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1)
 
     # Linear Regression Model
+    """
+    print("Linear Regression Model")
     W, b = linearRegression(X_train, y_train)
     y_pred = predict_linear(W, b, X_test)
     compute_error(y_test, y_pred)
+    print("___________________________________")
+    print(y_test)
+    print("___________________________________")
+    print(y_pred)
+    
 
     # MLP Model
+    print("MLP Model")
     W1, b1, W2, b2 = MLP(X_train, y_train)
     y_pred = predict_MLP(W1, b1, W2, b2, X_test)
-
+    compute_error(y_test, y_pred)
+"""
 
 if __name__ == '__main__':
     main()
